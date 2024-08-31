dfe83080b67282699bcc4b8fb2fc8207d61080ec
DinhHuy2010 <huy04052010@outlook.com>
2024-08-31 13:09:10 +0700

    add reprfmt

---
+++ dhlibs/reprfmt/constants.py
@@ -0,0 +1,18 @@
+import types
+from typing import Callable, TypeVar
+
+NEVER_RENDER_TYPES: tuple[type, ...] = (
+    types.FunctionType,
+    types.MethodType,
+    types.BuiltinFunctionType,
+    types.WrapperDescriptorType,
+    types.MethodWrapperType,
+    types.MethodDescriptorType,
+    types.ClassMethodDescriptorType,
+)
+MAX_RECURSIVE_RENDER_OBJLEVEL = 10
+DispatchPredCallback = Callable[[object], bool]
+
+T = TypeVar("T")
+
+__all__ = ["NEVER_RENDER_TYPES", "T"]
