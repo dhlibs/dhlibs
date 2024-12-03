795c04f35a72e3de705130fc19b725950bb79f56
DinhHuy2010 <huy04052010@outlook.com>
2024-12-03 23:13:18 +0700

    add metadata to __init__.py

--- dhlibs/__init__.py
+++ dhlibs/__init__.py
@@ -23,3 +23,14 @@
 # SOFTWARE.
 
 """dhlibs - random libraries that you shouldn't care"""
+
+def _get_version():
+    import importlib.metadata
+    return importlib.metadata.version(__name__)
+
+__author__ = "DinhHuy2010"
+__copyright__ = "Copyright (c) 2024 DinhHuy2010 (https://github.com/DinhHuy2010)"
+__license__ = "MIT"
+__version__ = _get_version()
+
+del _get_version
